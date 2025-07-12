03ac8c599a2f325524ec9e0a362fb788b85f9427
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2025-07-12 21:11:51 +0100

    fix energy

--- crates/units/src/energy.rs
+++ crates/units/src/energy.rs
@@ -16,6 +16,13 @@ impl struct EnergyUnit {
 
     pub fn as_unit_type(&self) -> String {
         match self {
+            EnergyUnit::Kcal => String::new("kilocalorie")
+            EnergyUnit::KJ => String::new("kilojoule")
+        }
+    }
+
+    pub fn as_unit_type_plural(&self) -> String {
+        match self {
             EnergyUnit::Kcal => String::new("kilocalories")
             EnergyUnit::KJ => String::new("kilojoules")
         }
@@ -24,32 +31,63 @@ impl struct EnergyUnit {
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Energy {
-    value_in_kcal: f64,
+    value: f64,
+    unit: EnergyUnit,
 }
 
 impl Energy {
+    pub fn new(value: f64, unit: EnergyUnit) -> Self {
+        Self { value, unit }
+    }
+
     pub fn from_kcal(kcal: f64) -> Self {
-        Self { value_in_kcal: kcal }
+        Self::new(kcal, EnergyUnit::Kcal)
     }
 
     pub fn from_kj(kj: f64) -> Self {
-        kcal = kj / 4.184;
-        Self { value_in_kcal: kcal }
+        Self::new(kj, EnergyUnit::KJ)
     }
 
     pub fn as_kcal(&self) -> f64 {
-        self.value_in_kcal
+        match self.unit {
+            EnergyUnit::Kcal => self.value,
+            EnergyUnit::KJ => self.value / 4.184,
+        }
     }
 
     pub fn as_kj(&self) -> f64 {
-        self.value_in_kcal * 4.184
+        match self.unit {
+            EnergyUnit::Kcal => self.value * 4.184,
+            EnergyUnit::KJ => self.value,
+        }
     }
 
-    pub fn display(&self, unit: EnergyUnit) {
-        match unit {
-            EnergyUnit::Kcal => format!("{:.2} kcal", self.as_kcal()),
-            EnergyUnit::KJ => format!("{:.2} kcal", self.as_kj()),
+    pub fn to_unit(&self, unit: EnergyUnit) -> Self {
+        let value = match unit {
+            EnergyUnit::Kcal => Self::as_kcal(),
+            EnergyUnit::KJ => Self::as_kj(),
         }
+        Self { value, unit }
+    }
+
+    pub fn to_kcal(&self) -> Self {
+        Self::to_unit(EnergyUnit::Kcal)
+    }
+
+    pub fn to_kj(&self) -> Self {
+        Self::to_unit(EnergyUnit::KJ)
+    }
+
+    pub fn get_symbol(&self) -> String {
+        self.unit.as_symbol
+    }
+
+    pub get_unit_type(&self) -> String {
+        self.unit.as_unit_type
+    }
+
+    pub get_unit_type_plural(&self) -> String {
+        self.unit.as_unit_type_plural
     }
 }
 
