b6abe3e0fe076ba7074d3ae8a4a628bae75d291e
1twalters01 <1twalters01>
2025-07-16 00:26:58 +0100

    basic xtask structure

---
+++ xtask/README.md
@@ -0,0 +1,9 @@
+# xtask
+
+
+## Commands
+* View help: cargo run -p xtask -- --help
+* Setup Python: cargo run -p xtask -- setup-python
+* Compile SCSS to CSS (for gtk 4 for linux): cargo run -p xtask -- build-css
+* Parse SCSS to XAML (for WinUI 3 for Windows): cargo run -p xtask --build-xaml
+* Parse SCSS to SwiftUI Modifiers (for SwiftUI for Mac): cargo run -p xtask -- build-swiftui-modifiers
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,50 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    SetupPython,
+    BuildCSS,
+    BuildXaml,
+    BuildSwiftuiModifiers,
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::SetupPython => setup_python()?,
+        Commands::BuildCSS => build_css()?,
+        Commands::BuildXaml => build_xaml()?,
+        Commands::BuildSwiftuiModifiers => build_swiftui_modifiers()?,
+    }
+
+    Ok(())
+}
+
+fn setup_python() -> Result<()> {
+    println!("Setup Python");
+    Ok(())
+}
+
+fn build_css() -> Result<()> {
+    println!("Build CSS for GTK 4 on Linux");
+    Ok(())
+}
+
+fn build_xaml() -> Result<()> {
+    println!("Build XAML for WinUI 3 on Windows");
+    Ok(())
+}
+
+fn build_swiftui_modifiers() -> Result<()> {
+    println!("Build SwiftUI modifiers for SwiftUI on Mac");
+    Ok(())
 }
