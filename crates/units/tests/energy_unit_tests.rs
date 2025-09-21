8394ec00d3cee34a0d238876da93bb32276ac355
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2025-09-21 19:17:49 +0100

    Create energy_unit_tests.rs

---
+++ crates/units/tests/energy_unit_tests.rs
@@ -0,0 +1,26 @@
+use std::collections::BTreeSet;
+use units::energy::EnergyUnit;
+
+#[test]
+fn test_get_energy_unit_enumerations() {
+}
+
+#[test]
+fn test_get_symbols() {
+}
+
+#[test]
+fn test_get_unit_types() {
+}
+
+#[test]
+fn test_get_plural_unit_types() {
+}
+
+#[test]
+fn test_get_factor() {
+}
+
+#[test]
+fn test_from_str() {
+}
