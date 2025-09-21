256fc42aea59ee6d118107d8438de3cf212e0125
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2025-09-21 19:13:22 +0100

    Create volume_tests.rs

---
+++ crates/units/tests/volume_tests.rs
@@ -0,0 +1,61 @@
+use units::volume::{Volume, VolumeUnit};
+
+#[test]
+fn test_new_volume() {}
+
+#[test]
+fn test_volume_rounding() {}
+
+#[test]
+fn test_volume_as_fn() {}
+
+#[test]
+fn test_volume_to_unit() {}
+
+#[test]
+fn test_volume_to_fn() {}
+
+#[test]
+fn test_volume_is_zero() {}
+
+#[test]
+fn test_volume_is_negative() {}
+
+#[test]
+fn test_volume_get_value() {}
+
+#[test]
+fn test_volume_set_value() {}
+
+#[test]
+fn test_volume_get_unit() {}
+
+#[test]
+fn test_volume_set_unit() {}
+
+#[test]
+fn test_volume_get_symbol() {}
+
+#[test]
+fn test_volume_unit_type() {}
+
+#[test]
+fn test_volume_unit_type_plural() {}
+
+#[test]
+fn test_volume_to_string() {}
+
+#[test]
+fn test_volume_add() {}
+
+#[test]
+fn test_volume_subtract() {}
+
+#[test]
+fn test_volume_multiply() {}
+
+#[test]
+fn test_volume_divide() {}
+
+#[test]
+fn test_volume_partial_order() {}
