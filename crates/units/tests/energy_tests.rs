d3065cf4306d722c5f94e16b33dc03f58fbb8a59
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2025-09-21 19:26:02 +0100

    Create energy_tests.rs

---
+++ crates/units/tests/energy_tests.rs
@@ -0,0 +1,61 @@
+use units::energy::{Energy, EnergyUnit};
+
+#[test]
+fn test_new_energy() {}
+
+#[test]
+fn test_energy_rounding() {}
+
+#[test]
+fn test_energy_as_fn() {}
+
+#[test]
+fn test_energy_to_unit() {}
+
+#[test]
+fn test_energy_to_fn() {}
+
+#[test]
+fn test_energy_is_zero() {}
+
+#[test]
+fn test_energy_is_negative() {}
+
+#[test]
+fn test_energy_get_value() {}
+
+#[test]
+fn test_energy_set_value() {}
+
+#[test]
+fn test_energy_get_unit() {}
+
+#[test]
+fn test_energy_set_unit() {}
+
+#[test]
+fn test_energy_get_symbol() {}
+
+#[test]
+fn test_energy_unit_type() {}
+
+#[test]
+fn test_energy_unit_type_plural() {}
+
+#[test]
+fn test_energy_to_string() {}
+
+#[test]
+fn test_energy_add() {}
+
+#[test]
+fn test_energy_subtract() {}
+
+#[test]
+fn test_energy_multiply() {}
+
+#[test]
+fn test_energy_divide() {}
+
+#[test]
+fn test_energy_partial_order() {}
