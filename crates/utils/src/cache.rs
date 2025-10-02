c2f909a9043baa8768c9a57c528b0b4dd208d278
Tyler Walters <1twalters01@gmail.com>
2025-10-02 22:41:12 +0100

    sqlite connection

--- crates/utils/src/cache.rs
+++ crates/utils/src/cache.rs
@@ -1,7 +1,9 @@
-// use dotenv::dotenv;
-use std::{path::PathBuf, time::{SystemTime, UNIX_EPOCH}};
-use serde::{ Serialize, de::DeserializeOwned };
+use serde::{de::DeserializeOwned, Serialize};
 use sled::Db;
+use std::{
+    path::PathBuf,
+    time::{SystemTime, UNIX_EPOCH},
+};
 
 pub struct CacheService {
     db: Db,
@@ -9,12 +11,11 @@ pub struct CacheService {
 
 impl CacheService {
     pub fn new() -> sled::Result<Self> {
-        let workspace_root = std::env::var("WORKSPACE_ROOT").unwrap();
+        let workspace_root = std::env::var("WORKSPACE_ROOT").expect("WORKSPACE_ROOT must be set");
         let workspace_pathbuf = PathBuf::from(workspace_root);
         let db_location = workspace_pathbuf.join("sled");
-
-    // pub fn new(db_location: &str) -> sled::Result<Self> {
         let db = sled::open(db_location)?;
+
         Ok(Self { db })
     }
 
@@ -28,15 +29,21 @@ impl CacheService {
         value: V,
         duration_in_seconds: Option<u64>,
     ) -> sled::Result<()>
-    where 
+    where
         K: AsRef<[u8]>,
         V: Serialize,
     {
-        let serialized_value = serde_json::to_vec(&value)
-            .map_err(|e| sled::Error::ReportableBug(e.to_string()))?;
+        let serialized_value =
+            serde_json::to_vec(&value).map_err(|e| sled::Error::ReportableBug(e.to_string()))?;
 
         let expiry = match duration_in_seconds {
-            Some(duration) => Some(SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() + duration),
+            Some(duration) => Some(
+                SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs()
+                    + duration,
+            ),
             None => None,
         };
 
@@ -46,12 +53,12 @@ impl CacheService {
                 buf.extend(b"|");
                 buf.extend(serialized_value);
                 buf
-            },
+            }
             None => {
                 let mut buf = b"0|".to_vec();
                 buf.extend(serialized_value);
                 buf
-            },
+            }
         };
 
         self.db.insert(key, stored_value)?;
@@ -87,33 +94,60 @@ impl CacheService {
                     let value: V = serde_json::from_slice(value_bytes)
                         .map_err(|e| sled::Error::ReportableBug(e.to_string()))?;
                     return Ok(Some(value));
-                } else { return Ok(None) }
-            },
+                } else {
+                    return Ok(None);
+                }
+            }
             None => Ok(None),
         }
     }
 
     pub fn delete_key<K>(&self, key: K) -> sled::Result<()>
     where
-            K: AsRef<[u8]>,
+        K: AsRef<[u8]>,
     {
         self.db.remove(key)?;
+        Ok(())
+    }
+
+    pub fn remove_expired(&self) -> sled::Result<()> {
+        let now = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_secs();
+
+        let mut batch = sled::Batch::default();
+        for item in self.db.iter() {
+            let (key, value) = item?;
+
+            if let Some(pos) = value.iter().position(|&b| b == b'|') {
+                let expiry_str = std::str::from_utf8(&value[..pos]).unwrap_or("0");
+                let expiry: u64 = expiry_str.parse().unwrap_or(0);
+
+                if expiry > 0 && now > expiry {
+                    batch.remove(key);
+                }
+            }
+        }
+
+        self.db.apply_batch(batch)?;
+        self.db.flush()?;
         Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{thread::sleep, time::Duration};
-
     use super::*;
-    use dotenv::dotenv;
+    use std::{thread::sleep, time::Duration};
 
     #[test]
     fn test_create_db_connection() {
         let cache = CacheService::new().expect("failed to open sled");
 
-        cache.store_key_value("ping", "pong", None).expect("insert failed");
+        cache
+            .store_key_value("ping", "pong", None)
+            .expect("insert failed");
         let result: Option<String> = cache.get_value("ping").expect("read failed");
 
         assert_eq!(result, Some("pong".to_string()));
@@ -122,9 +156,6 @@ mod tests {
 
     #[test]
     fn test_new_cache_service() {
-        dotenv().ok();
-
-        // let cache_service = CacheService::new().unwrap();
         let db = sled::Config::new().temporary(true).open().unwrap();
         let cache_service = CacheService::from_db(db);
 
@@ -143,10 +174,6 @@ mod tests {
 
     #[test]
     fn test_from_cache_service() {
-        dotenv().ok();
-
-        // let con = create_redis_client_connection();
-        // let mut cache_service = CacheService::from_con(con);
         let db = sled::Config::new().temporary(true).open().unwrap();
         let cache_service = CacheService::from_db(db);
 
@@ -162,5 +189,12 @@ mod tests {
 
         res = cache_service.get_value(key);
         assert_eq!(None, res.unwrap());
+    }
+
+    #[test]
+    fn test_clean_db() {
+        let db = sled::Config::new().temporary(true).open().unwrap();
+        let cache_service = CacheService::from_db(db);
+        cache_service.remove_expired().unwrap();
     }
 }
---
+++ crates/utils/src/database.rs
@@ -0,0 +1,58 @@
+use sqlx::{sqlite::SqlitePoolOptions, Pool, Sqlite};
+use std::{
+    fs,
+    path::PathBuf
+};
+
+pub struct DatabaseService {
+    pub pool: Pool<Sqlite>,
+}
+
+impl DatabaseService {
+    pub async fn from_url(database_url: &str) -> Result<Self, sqlx::Error> {
+        let pool = SqlitePoolOptions::new()
+            .max_connections(5)
+            .connect(database_url)
+            .await?;
+
+        Ok(Self { pool })
+    }
+
+    pub async fn new() -> Result<Self, sqlx::Error> {
+        let workspace_root = std::env::var("WORKSPACE_ROOT").expect("WORKSPACE_ROOT must be set");
+        let workspace_pathbuf = PathBuf::from(workspace_root);
+        let database_path = workspace_pathbuf.join("eiyodex.sqlite");
+
+        if !database_path.exists() {
+            fs::File::create(&database_path).expect("failed to create sqlite file");
+        }
+
+        let database_url = format!("sqlite://{}", database_path.to_str().unwrap());
+
+        let pool = SqlitePoolOptions::new()
+            .max_connections(5)
+            .connect(&database_url)
+            .await?;
+
+        Ok(Self { pool })
+    }
+
+    pub async fn ping(&self) -> Result<(), sqlx::Error> {
+        sqlx::query("SELECT 1")
+            .execute(&self.pool)
+            .await
+            .map(|_| ())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn test_create_database_session() {
+        let database = DatabaseService::new().await.expect("failed to open sqlite");
+        let res = database.ping().await;
+        assert!(res.is_ok());
+    }
+}
