65b7fa77f6a27924b21facd03dc30a7769866746
Tyler Walters <1twalters01@gmail.com>
2026-02-25 02:43:55 +0000

    initialise migrations

--- migrations/nutrients/0001_create_essentiality_types.sql
+++ migrations/nutrients/0001_create_essentiality_types.sql
@@ -1,9 +1,9 @@
-CREATE TABLE essentiality_types (
+CREATE TABLE IF NOT EXISTS essentiality_types (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL UNIQUE
 );
 
-INSERT INTO essentiality_types (id, name) VALUES
+INSERT OR IGNORE INTO essentiality_types (id, name) VALUES
 (1, 'essential'),
 (2, 'conditionally_essential'),
 (3, 'non_essential');
