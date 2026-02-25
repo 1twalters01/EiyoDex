5cf991e65538f3b195176d2f935aa4183c7f3965
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2026-02-25 12:46:31 +0000

    Rename quantity_types table to nutrients_quantity_types

--- migrations/nutrients/0002_create_quantity_types.sql
+++ migrations/nutrients/0002_create_quantity_types.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS quantity_types (
+CREATE TABLE IF NOT EXISTS nutrients_quantity_types (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL UNIQUE
 );
 
-INSERT OR IGNORE INTO quantity_types (id, name) VALUES
+INSERT OR IGNORE INTO nutrients_quantity_types (id, name) VALUES
 (1, 'macronutrient'),
 (2, 'micronutrient'),
 (3, 'non_nutrient');
