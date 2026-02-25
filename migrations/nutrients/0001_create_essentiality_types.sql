18d5c41e1eb58406ac1cd699772989a9377dde59
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2026-02-25 12:45:57 +0000

    Rename essentiality_types table to nutrients_essentiality_types

--- migrations/nutrients/0001_create_essentiality_types.sql
+++ migrations/nutrients/0001_create_essentiality_types.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS essentiality_types (
+CREATE TABLE IF NOT EXISTS nutrients_essentiality_types (
     id INTEGER PRIMARY KEY,
     name TEXT NOT NULL UNIQUE
 );
 
-INSERT OR IGNORE INTO essentiality_types (id, name) VALUES
+INSERT OR IGNORE INTO mutrients_essentiality_types (id, name) VALUES
 (1, 'essential'),
 (2, 'conditionally_essential'),
 (3, 'non_essential');
