31ba11a53f1210c42c8c100bfb2edb119592a423
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2026-03-01 13:44:01 +0000

    Add nutrient unit types and unit table
    Create nutrient unit types and unit table with foreign key constraints.

---
+++ migrations/nutrients/0009_create_nutrient_unit_table.sql
@@ -0,0 +1,43 @@
+CREATE TABLE IF NOT EXISTS nutrients_unit_types (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL UNIQUE
+);
+
+INSERT OR IGNORE INTO nutrients_sterol_types (id, name) VALUES
+(1, 'mass'),
+(2, 'volume'),
+(3, 'energy'),
+(4, 'iu'),
+(5, 'dfe'),
+(6, 'ne'),
+(7, 'rae'),
+(8, 'pdcaas'),
+(9, 'diaas1'),
+(10, 'diaas2'),
+(11, 'diass3');
+
+CREATE TABLE IF NOT EXISTS nutrients_unit_table (
+  id INTEGER PRIMARY KEY,
+  unit_type_id INTEGER NOT NULL,
+  mass_id INTEGER,
+  volume_id INTEGER,
+  energy_id INTEGER,
+  FOREIGN KEY (unit_type_id)
+        REFERENCES nutrients_unit_types(id)
+        ON DELETE CASCADE,
+  FOREIGN KEY (mass_id)
+        REFERENCES units_mass_types(id)
+        ON DELETE CASCADE,
+  FOREIGN KEY (volume_id)
+        REFERENCES units_volume_types(id)
+        ON DELETE CASCADE,
+  FOREIGN KEY (energy_id)
+        REFERENCES units_energy_types(id)
+        ON DELETE CASCADE,
+  CHECK (
+        (mass_id IS NOT NULL) +
+        (volume_id IS NOT NULL) +
+        (energy_id IS NOT NULL)
+        = 1
+  )
+)
