000ce57968148f4d517787a4b90bef5b0dde2cfc
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2026-03-02 23:16:38 +0000

    Create nutrient lists and items tables
    Create tables for nutrient lists and their items with foreign key constraints.

---
+++ migrations/nutrients/0013_create_nutrient_list_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS nutrients_nutrient_lists (
+    id INTEGER PRIMARY KEY,
+);
+
+CREATE TABLE IF NOT EXISTS nutrients_nutrient_list_items (
+    id INTEGER PRIMARY KEY,
+    nutrient_list_id NOT NULL,
+    nutrient_id INTEGER NOT NULL,
+    FOREIGN KEY (nutrient_list_id)
+        REFERENCES nutrients_nutrient_lists(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (output_id)
+        REFERENCES nutrients_nutrient_table(id)
+        ON DELETE CASCADE
+);
