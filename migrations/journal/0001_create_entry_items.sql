5d18c608efd26455358e638a6fd6ddee1b06b76a
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2026-03-03 01:09:11 +0000

    Create exercises_exercise_table migration

---
+++ migrations/journal/0001_create_entry_items.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS exercises_exercise_table (
+    id INTEGER PRIMary key,
+    food_quantity_id INTEGER,
+    exercise_quantity_id INTEGER,
+    FOREIGN KEY (food_quantity_id)
+        REFERENCES foods_food_quantities(id)
+        ON DELETE CASCADE,
+    FOREIGN KEY (exercise_quantity_id)
+        REFERENCES exercises_exercise_quantities(id)
+        ON DELETE CASCADE,
+);
