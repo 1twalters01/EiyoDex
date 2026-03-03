b6a9846399eab29b98168a9552e2fe1dfdeb3233
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2026-03-03 01:22:52 +0000

    Rename exercise table to journal_exercise_table

--- migrations/journal/0001_create_entry_items.sql
+++ migrations/journal/0001_create_entry_items.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS exercises_exercise_table (
+CREATE TABLE IF NOT EXISTS journal_exercise_table (
     id INTEGER PRIMary key,
     food_quantity_id INTEGER,
     exercise_quantity_id INTEGER,
