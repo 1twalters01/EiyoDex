ccd49fb3d7d90e461738293a54534a732574073d
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2026-03-03 01:16:56 +0000

    Create exercises_exercise_table migration

---
+++ migrations/journal/0002_create_entry_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS exercises_exercise_table (
+    id INTEGER PRIMARY key,
+    note TEXT,
+    entry_item_id INTEGER NOT NULL,
+    datetime_performed
+    datetime_created NOT NULL,
+    datetime_last_modified NOT NULL
+)
