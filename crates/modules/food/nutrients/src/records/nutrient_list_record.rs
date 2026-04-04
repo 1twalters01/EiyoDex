1ff3ee4402806e439c58d5282d7dd137e20eb1cd
Tyler Walters <1twalters01@gmail.com>
2026-04-04 20:05:39 +0100

    nutrient list record tests

--- crates/modules/food/nutrients/src/records/nutrient_list_record.rs
+++ crates/modules/food/nutrients/src/records/nutrient_list_record.rs
@@ -4,14 +4,19 @@ use uuid::Uuid;
 use crate::{
     nutrient::Nutrient,
     nutrient_list::NutrientList,
-    records::nutrient_record::{NutrientConversionsRecord, NutrientLinkRecordUuid, NutrientRecord},
+    records::nutrient_record::{NutrientConversionRecord, NutrientLinkRecordUuid, NutrientRecord},
 };
 
+#[derive(Debug, PartialEq)]
 pub struct NutrientListRecord {
     id: Vec<u8>,
 }
 
 impl NutrientListRecord {
+    pub fn from_value(id: Vec<u8>) -> Self {
+        Self { id }
+    }
+
     pub fn from_nutrient_list(nutrient_list: NutrientList) -> Self {
         let id = nutrient_list.get_id().as_bytes().to_vec();
         Self { id }
@@ -20,7 +25,12 @@ impl NutrientListRecord {
     pub fn to_nutrient_quantity_list(&self) -> NutrientList {
         let mut nutrient_list = NutrientList::new();
         nutrient_list.set_id(Uuid::from_slice(&self.id).unwrap());
+
         return nutrient_list;
+    }
+
+    pub fn get_id(&self) -> Vec<u8> {
+        self.id.clone()
     }
 
     pub async fn save_to_database(&self, pool: &Pool<Sqlite>) -> Result<(), sqlx::Error> {
@@ -47,10 +57,6 @@ impl NutrientListRecord {
 
         Ok(())
     }
-
-    pub fn get_id(&self) -> Vec<u8> {
-        self.id.clone()
-    }
 }
 
 pub struct NutrientListItemRecord {
@@ -59,204 +65,207 @@ pub struct NutrientListItemRecord {
 }
 
 impl NutrientListItemRecord {
-    pub async fn from_nutrient_list(nutrient_list: NutrientList) -> Vec<Self> {
+    pub async fn from_nutrient_list(nutrient_list: NutrientList, pool: &Pool<Sqlite>) -> Result<Vec<Self>, sqlx::Error> {
         let nutrient_list_id = nutrient_list.get_id().as_bytes().to_vec();
+        let mut nutrient_list_item_vec: Vec<Self> = Vec::new();
 
-        nutrient_list
-            .get_nutrients()
-            .iter()
-            .map(|nutrient| Self {
+        for nutrient in nutrient_list.get_nutrients() {
+            let nutrient_id = NutrientRecord::from_nutrient(nutrient.borrow().clone(), pool).await.unwrap().nutrient_id;
+            let item = Self {
                 nutrient_list_id: nutrient_list_id.clone(),
-                nutrient_id: nutrient.get_id().as_bytes().to_vec(),
-            })
-            .collect()
-    }
-
-    pub async fn to_nutrient_vec_from_from_vec(
-        items: Vec<&Self>,
-        pool: &Pool<Sqlite>,
-    ) -> Result<Vec<(Nutrient, NutrientLinkRecordUuid)>, sqlx::Error> {
-        let mut tx = pool.begin().await?;
-
-        let mut nutrient_map: Vec<(Nutrient, NutrientLinkRecordUuid)> = Vec::new();
-        for item in items {
-            let nutrient_record = sqlx::query_as!(
-                NutrientRecord,
-                r#"
-                    SELECT
-                        id as nutrient_id,
-                        name,
-                        description,
-                        main_unit_id,
-                        quantity_type_id,
-                        essentiality_type_id,
-                        chemical_id
-                    FROM nutrients_nutrient_table
-                    WHERE
-                        id = ?
-                "#,
-                item.nutrient_id
-            )
-            .fetch_one(&mut *tx)
-            .await?;
-
-            let conversion_vec = sqlx::query_as!(
-                NutrientConversionsRecord,
-                r#"
-                    SELECT
-                        nutrient_id,
-                        unit_id,
-                        factor
-                    FROM nutrients_unit_conversions
-                    WHERE
-                        nutrient_id = ?
-                "#,
-                item.nutrient_id
-            )
-            .fetch_all(&mut *tx)
-            .await?;
-            let unit_conversions =
-                NutrientConversionsRecord::to_btree_map_from_vec(conversion_vec, pool).await;
-
-            let parent_rows = sqlx::query!(
-                r#"
-                SELECT
-                    parent_id
-                FROM nutrients_nutrient_relationships
-                WHERE
-                    child_id = ?
-            "#,
-                item.nutrient_id
-            )
-            .fetch_all(&mut *tx)
-            .await?;
-            let parent_id_vec: Vec<Uuid> = parent_rows
-                .iter()
-                .map(|row| Uuid::from_slice(&row.parent_id).unwrap())
-                .collect();
-
-            let child_rows = sqlx::query!(
-                r#"
-                SELECT
-                    child_id
-                FROM nutrients_nutrient_relationships
-                WHERE
-                    parent_id = ?
-            "#,
-                item.nutrient_id
-            )
-            .fetch_all(&mut *tx)
-            .await?;
-            let child_id_vec: Vec<Uuid> = child_rows
-                .iter()
-                .map(|row| Uuid::from_slice(&row.child_id).unwrap())
-                .collect();
-
-            let nutrient_id = Uuid::from_slice(&item.nutrient_id).unwrap();
-            let nutrient_link_record_uuid = NutrientLinkRecordUuid {
                 nutrient_id,
-                parent_id_vec,
-                child_id_vec,
             };
-
-            let mut nutrient = nutrient_record.to_nutrient(pool).await;
-            nutrient.set_unit_conversions(unit_conversions);
-            nutrient_map.push((nutrient, nutrient_link_record_uuid));
+            nutrient_list_item_vec.push(item);
         }
 
-        let _ = tx.commit();
-
-        Ok(nutrient_map)
+        return Ok(nutrient_list_item_vec)
     }
 
-    pub async fn load_from_sqlite(nutrient_list_id: Uuid, pool: &Pool<Sqlite>) -> Result<Vec<Self>, sqlx::Error> {
-        Ok(sqlx::query_as!(
-            NutrientListItemRecord,
-            r#"
-                    SELECT
-                        nutrient_list_id,
-                        nutrient_id
-                    FROM nutrients_nutrient_list_items
-                    WHERE
-                        nutrient_list_id = ?
-                "#,
-            nutrient_list_id
-        )
-        .fetch_all(pool)
-        .await?)
-    }
-
-    pub async fn save_to_database(&self, pool: &Pool<Sqlite>) -> Result<(), sqlx::Error> {
-        sqlx::query!(
-            r#"
-                INSERT INTO nutrients_nutrient_list_items (nutrient_list_id, nutrient_id)
-                VALUES (?, ?)
-                ON CONFLICT DO NOTHING
-            "#,
-            self.nutrient_list_id,
-            self.nutrient_id,
-        )
-        .execute(pool)
-        .await?;
-        Ok(())
-    }
-
-    pub async fn save_vec_to_database(items: Vec<&Self>, pool: &Pool<Sqlite>) -> Result<(), sqlx::Error> {
-        let mut tx = pool.begin().await?;
-
-        for item in items {
-            sqlx::query!(
-                r#"
-                    INSERT INTO nutrients_nutrient_list_items (nutrient_list_id, nutrient_id)
-                    VALUES (?, ?)
-                    ON CONFLICT DO NOTHING
-                "#,
-                item.nutrient_list_id,
-                item.nutrient_id,
-            )
-            .execute(&mut *tx)
-            .await?;
-        }
-        Ok(())
-    }
-
-    pub async fn delete_conversion(&self, pool: &Pool<Sqlite>) -> Result<(), sqlx::Error> {
-        sqlx::query!(
-            r#"
-                DELETE FROM nutrients_nutrient_list_items 
-                WHERE
-                    nutrient_list_id = ?
-                    AND nutrient_id = ?
-            "#,
-            self.nutrient_list_id,
-            self.nutrient_id,
-        )
-        .execute(pool)
-        .await?;
-
-        Ok(())
-    }
-
-    pub async fn delete_conversion_vec(items: Vec<&Self>, pool: &Pool<Sqlite>) -> Result<(), sqlx::Error> {
-        let mut tx = pool.begin().await?;
-
-        for item in items {
-            sqlx::query!(
-                r#"
-                    DELETE FROM nutrients_nutrient_list_items
-                    WHERE
-                        nutrient_list_id = ?
-                        AND nutrient_id = ?
-                "#,
-                item.nutrient_list_id,
-                item.nutrient_id,
-            )
-            .execute(&mut *tx)
-            .await?;
-        }
-
-        let _ = tx.commit();
-
-        Ok(())
-    }
+//     pub async fn to_nutrient_vec_from_from_vec(
+//         items: Vec<&Self>,
+//         pool: &Pool<Sqlite>,
+//     ) -> Result<Vec<(Nutrient, NutrientLinkRecordUuid)>, sqlx::Error> {
+//         let mut tx = pool.begin().await?;
+//
+//         let mut nutrient_map: Vec<(Nutrient, NutrientLinkRecordUuid)> = Vec::new();
+//         for item in items {
+//             let nutrient_record = sqlx::query_as!(
+//                 NutrientRecord,
+//                 r#"
+//                     SELECT
+//                         id as nutrient_id,
+//                         name,
+//                         description,
+//                         main_unit_id,
+//                         quantity_type_id,
+//                         essentiality_type_id,
+//                         chemical_id
+//                     FROM nutrients_nutrient_table
+//                     WHERE
+//                         id = ?
+//                 "#,
+//                 item.nutrient_id
+//             )
+//             .fetch_one(&mut *tx)
+//             .await?;
+//
+//             let conversion_vec = sqlx::query_as!(
+//                 NutrientConversionRecord,
+//                 r#"
+//                     SELECT
+//                         nutrient_id,
+//                         unit_id,
+//                         factor
+//                     FROM nutrients_unit_conversions
+//                     WHERE
+//                         nutrient_id = ?
+//                 "#,
+//                 item.nutrient_id
+//             )
+//             .fetch_all(&mut *tx)
+//             .await?;
+//             let unit_conversions =
+//                 NutrientConversionRecord::to_btree_map_from_vec(conversion_vec, pool).await;
+//
+//             let parent_rows = sqlx::query!(
+//                 r#"
+//                 SELECT
+//                     parent_id
+//                 FROM nutrients_nutrient_relationships
+//                 WHERE
+//                     child_id = ?
+//             "#,
+//                 item.nutrient_id
+//             )
+//             .fetch_all(&mut *tx)
+//             .await?;
+//             let parent_id_vec: Vec<Uuid> = parent_rows
+//                 .iter()
+//                 .map(|row| Uuid::from_slice(&row.parent_id).unwrap())
+//                 .collect();
+//
+//             let child_rows = sqlx::query!(
+//                 r#"
+//                 SELECT
+//                     child_id
+//                 FROM nutrients_nutrient_relationships
+//                 WHERE
+//                     parent_id = ?
+//             "#,
+//                 item.nutrient_id
+//             )
+//             .fetch_all(&mut *tx)
+//             .await?;
+//             let child_id_vec: Vec<Uuid> = child_rows
+//                 .iter()
+//                 .map(|row| Uuid::from_slice(&row.child_id).unwrap())
+//                 .collect();
+//
+//             let nutrient_id = Uuid::from_slice(&item.nutrient_id).unwrap();
+//             let nutrient_link_record_uuid = NutrientLinkRecordUuid {
+//                 nutrient_id,
+//                 parent_id_vec,
+//                 child_id_vec,
+//             };
+//
+//             let mut nutrient = nutrient_record.to_nutrient(pool).await;
+//             nutrient.set_unit_conversions(unit_conversions);
+//             nutrient_map.push((nutrient, nutrient_link_record_uuid));
+//         }
+//
+//         let _ = tx.commit();
+//
+//         Ok(nutrient_map)
+//     }
+//
+//     pub async fn load_all_from_sqlite(nutrient_list_id: Uuid, pool: &Pool<Sqlite>) -> Result<Vec<Self>, sqlx::Error> {
+//         Ok(sqlx::query_as!(
+//             NutrientListItemRecord,
+//             r#"
+//                     SELECT
+//                         nutrient_list_id,
+//                         nutrient_id
+//                     FROM nutrients_nutrient_list_items
+//                     WHERE
+//                         nutrient_list_id = ?
+//                 "#,
+//             nutrient_list_id
+//         )
+//         .fetch_all(pool)
+//         .await?)
+//     }
+//
+//     pub async fn save_to_database(&self, pool: &Pool<Sqlite>) -> Result<(), sqlx::Error> {
+//         sqlx::query!(
+//             r#"
+//                 INSERT INTO nutrients_nutrient_list_items (nutrient_list_id, nutrient_id)
+//                 VALUES (?, ?)
+//                 ON CONFLICT DO NOTHING
+//             "#,
+//             self.nutrient_list_id,
+//             self.nutrient_id,
+//         )
+//         .execute(pool)
+//         .await?;
+//         Ok(())
+//     }
+//
+//     pub async fn save_vec_to_database(items: Vec<&Self>, pool: &Pool<Sqlite>) -> Result<(), sqlx::Error> {
+//         let mut tx = pool.begin().await?;
+//
+//         for item in items {
+//             sqlx::query!(
+//                 r#"
+//                     INSERT INTO nutrients_nutrient_list_items (nutrient_list_id, nutrient_id)
+//                     VALUES (?, ?)
+//                     ON CONFLICT DO NOTHING
+//                 "#,
+//                 item.nutrient_list_id,
+//                 item.nutrient_id,
+//             )
+//             .execute(&mut *tx)
+//             .await?;
+//         }
+//         Ok(())
+//     }
+//
+//     pub async fn delete_conversion(&self, pool: &Pool<Sqlite>) -> Result<(), sqlx::Error> {
+//         sqlx::query!(
+//             r#"
+//                 DELETE FROM nutrients_nutrient_list_items 
+//                 WHERE
+//                     nutrient_list_id = ?
+//                     AND nutrient_id = ?
+//             "#,
+//             self.nutrient_list_id,
+//             self.nutrient_id,
+//         )
+//         .execute(pool)
+//         .await?;
+//
+//         Ok(())
+//     }
+//
+//     pub async fn delete_conversion_vec(items: Vec<&Self>, pool: &Pool<Sqlite>) -> Result<(), sqlx::Error> {
+//         let mut tx = pool.begin().await?;
+//
+//         for item in items {
+//             sqlx::query!(
+//                 r#"
+//                     DELETE FROM nutrients_nutrient_list_items
+//                     WHERE
+//                         nutrient_list_id = ?
+//                         AND nutrient_id = ?
+//                 "#,
+//                 item.nutrient_list_id,
+//                 item.nutrient_id,
+//             )
+//             .execute(&mut *tx)
+//             .await?;
+//         }
+//
+//         let _ = tx.commit();
+//
+//         Ok(())
+//     }
 }
--- crates/modules/food/nutrients/tests/nutrient_list_record_tests.rs
+++ crates/modules/food/nutrients/tests/nutrient_list_record_tests.rs
@@ -1,20 +1,192 @@
-#[test]
-fn test_from_nutrient_list() {
+use std::collections::HashMap;
+
+use identity::{inner_id::InnerIdType, Id};
+use nutrients::{
+    entity::Entity, nutrient::{link_parent_child, Nutrient}, nutrient_list::NutrientList, nutrient_units::NutrientUnit, records::{nutrient_list_record::NutrientListRecord, nutrient_record::{NutrientLinkRecord, NutrientRecord}, nutrient_type_record::NutrientTypeRecord, nutrient_unit_record::NutrientUnitRecord}, schema::{nutrient_classes::{ChemicalType, EssentialityType, QuantityType}, nutrient_type::NutrientType}};
+use units::{energy::unit::EnergyUnit, mass::unit::MassUnit, volume::unit::VolumeUnit};
+use utils::database::DatabaseService;
+
+#[tokio::test]
+async fn test_from_nutrient_list() {
+    let pool = DatabaseService::new().await.unwrap().get_pool();
+    let _ = MassUnit::save_enumerations_to_database(&pool).await;
+    let _ = VolumeUnit::save_enumerations_to_database(&pool).await;
+    let _ = EnergyUnit::save_enumerations_to_database(&pool).await;
+    let _ = NutrientUnitRecord::save_enumerations_to_database(&pool).await;
+
+    let mut nutrient_list = NutrientList::new();
+    
+    let nutrient_type = NutrientType {
+        chemical_type: ChemicalType::Mineral,
+        quantity_type: QuantityType::Micronutrient,
+        essentiality_type: Some(EssentialityType::Essential),
+    };
+    let main_unit = NutrientUnit::Mass(MassUnit::Milligram);
+
+    let main_unit_record = NutrientUnitRecord::from_nutrient_unit(main_unit, &pool).await;
+    let unit_res = main_unit_record.save_to_database(&pool).await;
+    assert!(unit_res.is_ok());
+
+    let nutrient_type_record = NutrientTypeRecord::from_nutrient_type(nutrient_type.clone());
+    let type_res = nutrient_type_record.save_to_database(&pool).await;
+    assert!(type_res.is_ok());
+
+    
+    // Create nutrients
+    let iron = Nutrient::new_rc_refcell(String::from("Iron"), nutrient_type.clone(), main_unit);
+    let heme_iron = Nutrient::new_rc_refcell(String::from("Heme Iron"), nutrient_type.clone(), main_unit);
+    let non_heme_iron = Nutrient::new_rc_refcell(String::from("Non-heme Iron"), nutrient_type.clone(), main_unit);
+    let non_heme_iron_a = Nutrient::new_rc_refcell(String::from("Non-heme Iron A"), nutrient_type.clone(), main_unit);
+    let non_heme_iron_b = Nutrient::new_rc_refcell(String::from("Non-heme Iron B"), nutrient_type.clone(), main_unit);
+
+
+    // link nutrients
+    link_parent_child(&iron, &heme_iron).unwrap();
+    link_parent_child(&iron, &non_heme_iron).unwrap();
+    link_parent_child(&non_heme_iron, &non_heme_iron_a).unwrap();
+    link_parent_child(&non_heme_iron, &non_heme_iron_b).unwrap();
+
+
+    // Create entities from nutrients
+    let mut iron_entity = Entity::new(iron.borrow().clone());
+    let mut heme_iron_entity = Entity::new(heme_iron.borrow().clone());
+    let mut non_heme_iron_entity = Entity::new(non_heme_iron.borrow().clone());
+    let mut non_heme_iron_a_entity = Entity::new(non_heme_iron_a.borrow().clone());
+    let mut non_heme_iron_b_entity = Entity::new(non_heme_iron_b.borrow().clone());
+
+
+    // Get real entity ids
+    let iron_record = NutrientRecord::from_nutrient_entity(iron_entity.clone(), &pool).await.unwrap();
+    let iron_entity_id = iron_record.save_to_database(&pool).await.unwrap();
+
+    let heme_iron_record = NutrientRecord::from_nutrient_entity(heme_iron_entity.clone(), &pool).await.unwrap();
+    let heme_iron_entity_id = heme_iron_record.save_to_database(&pool).await.unwrap();
+
+    let non_heme_iron_record = NutrientRecord::from_nutrient_entity(non_heme_iron_entity.clone(), &pool).await.unwrap();
+    let non_heme_iron_entity_id = non_heme_iron_record.save_to_database(&pool).await.unwrap();
+
+    let non_heme_iron_a_record = NutrientRecord::from_nutrient_entity(non_heme_iron_a_entity.clone(), &pool).await.unwrap();
+    let non_heme_iron_a_entity_id = non_heme_iron_a_record.save_to_database(&pool).await.unwrap();
+
+    let non_heme_iron_b_record = NutrientRecord::from_nutrient_entity(non_heme_iron_b_entity.clone(), &pool).await.unwrap();
+    let non_heme_iron_b_entity_id = non_heme_iron_b_record.save_to_database(&pool).await.unwrap();
+
+
+    // update entity ids
+    iron_entity.set_id(Id::from_bytes(InnerIdType::Uuid, iron_entity_id.clone().try_into().unwrap()));
+    heme_iron_entity.set_id(Id::from_bytes(InnerIdType::Uuid, heme_iron_entity_id.clone().try_into().unwrap()));
+    non_heme_iron_entity.set_id(Id::from_bytes(InnerIdType::Uuid, non_heme_iron_entity_id.clone().try_into().unwrap()));
+    non_heme_iron_a_entity.set_id(Id::from_bytes(InnerIdType::Uuid, non_heme_iron_a_entity_id.clone().try_into().unwrap()));
+    non_heme_iron_b_entity.set_id(Id::from_bytes(InnerIdType::Uuid, non_heme_iron_b_entity_id.clone().try_into().unwrap()));
+
+
+    // Create link records
+    let (mut iron_link_record, iron_link_hashes) = NutrientLinkRecord::from_nutrient_entity(iron_entity.clone(), &pool).await.unwrap();
+    let (mut heme_iron_link_record, heme_iron_link_hashes) = NutrientLinkRecord::from_nutrient_entity(heme_iron_entity.clone(), &pool).await.unwrap();
+    let (mut non_heme_iron_link_record, non_heme_iron_link_hashes) = NutrientLinkRecord::from_nutrient_entity(non_heme_iron_entity.clone(), &pool).await.unwrap();
+    let (mut non_heme_iron_a_link_record, non_heme_iron_a_link_hashes) = NutrientLinkRecord::from_nutrient_entity(non_heme_iron_a_entity.clone(), &pool).await.unwrap();
+    let (mut non_heme_iron_b_link_record, non_heme_iron_b_link_hashes) = NutrientLinkRecord::from_nutrient_entity(non_heme_iron_b_entity.clone(), &pool).await.unwrap();
+
+
+    // Get nutrient entity Vec
+    let nutrient_name_new_id_map: HashMap<String, Vec<u8>> = HashMap::from([
+        (iron_entity.clone().get_inner().get_name(), iron_entity_id.clone()),
+        (heme_iron_entity.clone().get_inner().get_name(), heme_iron_entity_id.clone()),
+        (non_heme_iron_entity.clone().get_inner().get_name(), non_heme_iron_entity_id.clone()),
+        (non_heme_iron_a_entity.clone().get_inner().get_name(), non_heme_iron_a_entity_id.clone()),
+        (non_heme_iron_b_entity.clone().get_inner().get_name(), non_heme_iron_b_entity_id.clone()),
+    ]);
+
+
+    // Update link records with new ids
+    iron_link_record = iron_link_record.update_nutrient_link_ids(&iron_link_hashes, &nutrient_name_new_id_map);
+    heme_iron_link_record = heme_iron_link_record.update_nutrient_link_ids(&heme_iron_link_hashes, &nutrient_name_new_id_map);
+    non_heme_iron_link_record = non_heme_iron_link_record.update_nutrient_link_ids(&non_heme_iron_link_hashes, &nutrient_name_new_id_map);
+    non_heme_iron_a_link_record = non_heme_iron_a_link_record.update_nutrient_link_ids(&non_heme_iron_a_link_hashes, &nutrient_name_new_id_map);
+    non_heme_iron_b_link_record = non_heme_iron_b_link_record.update_nutrient_link_ids(&non_heme_iron_b_link_hashes, &nutrient_name_new_id_map);
+
+
+    // Add nutrients to nutrient list
+    nutrient_list.push(iron);
+    nutrient_list.push(heme_iron);
+    nutrient_list.push(non_heme_iron);
+    nutrient_list.push(non_heme_iron_a);
+    nutrient_list.push(non_heme_iron_b);
+
+
+    // Create nutrient list record
+    let nutrient_list_record = NutrientListRecord::from_nutrient_list(nutrient_list.clone());
+    let manual_list_record = NutrientListRecord::from_value(nutrient_list.get_id().as_bytes().to_vec());
+
+    assert_eq!(nutrient_list_record, manual_list_record);
 }
 
-#[test]
-fn test_to_nutrient_list() {
+#[tokio::test]
+async fn test_to_nutrient_list() {
+    let mut nutrient_list = NutrientList::new();
+    let nutrient_list_record = NutrientListRecord::from_nutrient_list(nutrient_list.clone());
+    let converted_list = nutrient_list_record.to_nutrient_quantity_list();
+
+    assert_eq!(nutrient_list.get_id(), converted_list.get_id())
 }
 
-#[test]
-fn test_save_to_database() {
-}
+#[tokio::test]
+async fn test_database_operations() {
+    let pool = DatabaseService::new().await.unwrap().get_pool();
+    let _ = MassUnit::save_enumerations_to_database(&pool).await;
+    let _ = VolumeUnit::save_enumerations_to_database(&pool).await;
+    let _ = EnergyUnit::save_enumerations_to_database(&pool).await;
+    let _ = NutrientUnitRecord::save_enumerations_to_database(&pool).await;
 
-#[test]
-fn test_load_from_database() {
-}
+    let mut nutrient_list = NutrientList::new();
+    
+    let nutrient_type = NutrientType {
+        chemical_type: ChemicalType::Mineral,
+        quantity_type: QuantityType::Micronutrient,
+        essentiality_type: Some(EssentialityType::Essential),
+    };
+    let main_unit = NutrientUnit::Mass(MassUnit::Milligram);
 
-#[test]
-fn test_delete_from_database() {
+    let main_unit_record = NutrientUnitRecord::from_nutrient_unit(main_unit, &pool).await;
+    let unit_res = main_unit_record.save_to_database(&pool).await;
+    assert!(unit_res.is_ok());
+
+    let nutrient_type_record = NutrientTypeRecord::from_nutrient_type(nutrient_type.clone());
+    let type_res = nutrient_type_record.save_to_database(&pool).await;
+    assert!(type_res.is_ok());
+
+    
+    // Create nutrients
+    let iron = Nutrient::new_rc_refcell(String::from("Iron"), nutrient_type.clone(), main_unit);
+    let heme_iron = Nutrient::new_rc_refcell(String::from("Heme Iron"), nutrient_type.clone(), main_unit);
+    let non_heme_iron = Nutrient::new_rc_refcell(String::from("Non-heme Iron"), nutrient_type.clone(), main_unit);
+    let non_heme_iron_a = Nutrient::new_rc_refcell(String::from("Non-heme Iron A"), nutrient_type.clone(), main_unit);
+    let non_heme_iron_b = Nutrient::new_rc_refcell(String::from("Non-heme Iron B"), nutrient_type.clone(), main_unit);
+
+
+    // link nutrients
+    link_parent_child(&iron, &heme_iron).unwrap();
+    link_parent_child(&iron, &non_heme_iron).unwrap();
+    link_parent_child(&non_heme_iron, &non_heme_iron_a).unwrap();
+    link_parent_child(&non_heme_iron, &non_heme_iron_b).unwrap();
+
+
+    // Add nutrients to nutrient list
+    nutrient_list.push(iron);
+    nutrient_list.push(heme_iron);
+    nutrient_list.push(non_heme_iron);
+    nutrient_list.push(non_heme_iron_a);
+    nutrient_list.push(non_heme_iron_b);
+
+
+    // Create nutrient list record
+    let nutrient_list_record = NutrientListRecord::from_nutrient_list(nutrient_list.clone());
+
+
+    // save and delete to db
+    let save_res = nutrient_list_record.save_to_database(&pool).await;
+    assert!(save_res.is_ok());
+    let delete_res = nutrient_list_record.delete_from_database(&pool).await;
+    assert!(delete_res.is_ok());
 }
 
