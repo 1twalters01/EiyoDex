3bb0d653bc46ad7e681b3fdf1787913eed78c55d
Tyler Walters <115738254+1twalters01@users.noreply.github.com>
2026-04-27 17:02:12 +0100

    Implement ExerciseList struct with methods

---
+++ crates/modules/exercise/exercise/src/exercise_list.rs
@@ -0,0 +1,83 @@
+use std::{cell::RefCell, rc::Rc};
+
+use uuid::Uuid;
+
+use crate::exercise::Exercise;
+
+#[derive(Debug, Clone)]
+pub struct ExerciseList {
+    id: Uuid,
+    name: String,
+    description: String,
+    exercises: Vec<Rc<RefCell<Exercise>>>,
+}
+
+impl NutrientList {
+    pub fn new() -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            name: String::new(),
+            description: String::new(),
+            exercises: Vec::new(),
+        }
+    }
+
+    pub fn from_vec(exercises: Vec<Rc<RefCell<Exercise>>>) -> Self {
+        Self {
+            id: Uuid::new_v4(),
+            name: String::new(),
+            description: String::new(),
+            exercises: exercises,
+        }
+    }
+
+    pub fn get_id(&self) -> Uuid {
+        self.id
+    }
+
+    pub fn set_id(&mut self, id: Uuid) {
+        self.id = id;
+    }
+
+    pub fn get_name(&self) -> String {
+        self.name.clone()
+    }
+
+    pub fn set_name(&mut self, name: String) {
+        self.name = name;
+    }
+
+    pub fn get_description(&self) -> String {
+        self.description.clone()
+    }
+
+    pub fn set_description(&mut self, description: String) {
+        self.description = description;
+    }
+
+    pub fn get_exercises(&self) -> Vec<Rc<RefCell<Exercise>>> {
+        self.exercises.clone()
+    }
+
+    pub fn set_exercises(&mut self, exercises: Vec<Rc<RefCell<Exercise>>>) {
+        self.exercises = exercises
+
+    pub fn get_exercise_names(&self) -> Vec<String> {
+        self.exercises
+            .iter()
+            .map(|exercise| exercise.borrow().get_name())
+            .collect()
+    }
+
+    pub fn push(&mut self, exercise: Rc<RefCell<Exercise>>) {
+        self.exercises.push(exercise)
+    }
+
+    pub fn extend(&mut self, exercises: Vec<Rc<RefCell<Exercise>>>) {
+        self.exercises.extend(exercises);
+    }
+
+    pub fn remove(&mut self, exercise: Rc<RefCell<Exercise>>) {
+        self.exercises.retain(|n| Rc::ptr_eq(n, exercise))
+    }
+}
